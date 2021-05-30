d945c1302fea150625974af78b1d739c614d8a0a
Aryamann Ningombam <aryamannsingh9@gmail.com>
2021-05-30 11:58:23 +0530

    added tier section

---
+++ client/src/Components/TierSection.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import './TierSection.scss';
+import card from '../images/madara.svg';
+import silverTorch from '../assets/Torches/silver-torch.svg'
+import bronzeTorch from '../assets/Torches/bronze-torch.svg'
+import goldTorch from '../assets/Torches/gold-torch.svg'
+import platinumTorch from '../assets/Torches/platinum-torch.svg'
+import { Link } from 'react-router-dom';
+
+export const TierSection = () => {
+
+
+
+    return (
+        <div id="tier-section-main-div">
+            <span id="main-heading">select your tier</span>
+
+            <div id="cards-section">
+                <Link to="/coming-soon">
+                    <div id="bronze-card-area" className="card-area">
+                        <img src={card} className="card-styling" id="bronze-card" />
+
+
+                        <img className="torch-styling" src={bronzeTorch} />
+                    </div>
+                </Link>
+
+                <Link to="/coming-soon">
+                    <div id="platinum-card-area" className="card-area">
+                        <img src={card} className="card-styling" id="platinum-card" />
+
+                        {/* <div className="card-text">
+                for the selected few, made with extreme precision and love
+            </div> */}
+                        <img className="torch-styling" src={platinumTorch} />
+                    </div>
+                </Link>
+                <Link to="/coming-soon">
+                    <div id="gold-card-area" className="card-area">
+                        <img id="gold-card" className="card-styling" src={card} />
+
+                        {/* <div className="card-text">
+                for the taste of high life
+            </div> */}
+                        <img className="torch-styling" src={goldTorch} />
+                    </div>
+                </Link>
+                <Link to="/coming-soon">
+                    <div id="silver-card-area" className="card-area">
+                        <img id="silver-card" className="card-styling" src={card} />
+
+                        {/* <div className="card-text">
+                a touch of silver in your cards collection
+            </div> */}
+                        <img className="torch-styling" src={silverTorch} />
+                    </div>
+                </Link>
+
+            </div>
+        </div>
+    )
+}
