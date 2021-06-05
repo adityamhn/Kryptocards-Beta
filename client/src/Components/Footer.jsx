09c96b0488b536b5e5479d4af64ead57d6beba25
Aryamann Ningombam <aryamannsingh9@gmail.com>
2021-06-05 16:55:40 +0530

    test

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Container,Button, FormControl, Form } from 'react-bootstrap'
+import { Container, Button, FormControl, Form } from 'react-bootstrap'
 import './Footer.scss'
 import * as yup from 'yup';
-import {Formik} from 'formik';
+import { Formik } from 'formik';
 import { SubscribeToNewsletter } from '../services/newsletter.service';
-import {showFormMessage} from '../util/util';
+import { showFormMessage } from '../util/util';
 const validationSchema = yup.object().shape({
     email: yup.string().email("Wrong email format!")
         .required("Required Field!")
@@ -12,96 +12,102 @@ const validationSchema = yup.object().shape({
 })
 
 const formInitialValues = {
- 
+
     email: '',
- 
+
 
 }
 
 
 const Footer = () => {
-    const onFormSubmit = (values)=>{
-        
-        SubscribeToNewsletter(values)
-        .then(response=>{
+    const onFormSubmit = (values) => {
 
-            showFormMessage("You Have Successfully! Subscribed!",'success');  
-        })
-        .catch(err=>{
-            console.log(err.message);
-            showFormMessage("There Was A Server Error, Please Try Again Later!",'error')
-        })
+        SubscribeToNewsletter(values)
+            .then(response => {
+
+                showFormMessage("You Have Successfully! Subscribed!", 'success');
+            })
+            .catch(err => {
+                console.log(err.message);
+                showFormMessage("There Was A Server Error, Please Try Again Later!", 'error')
+            })
 
     }
 
 
     return (
-        
+
         <Container fluid className="footer-sec-cont">
-            
-        <div className="wrapper">
-            <div className="header">
-            
-            <Formik
-                initialValues={formInitialValues}
-                validationSchema={validationSchema}
-                onSubmit={(values) => {
 
-                    onFormSubmit(values);
-                }}
-            >
-                {
-                    ({ handleChange, handleBlur, handleSubmit, values, touched, errors }) => {
+            <div className="wrapper">
+                <div className="header">
 
-                        return (
-            <Form  className="form-sec">
-            <Form.Label className="heading">GET OUR LATEST <span className="other-color">UPDATES</span></Form.Label>
-            <div className="column">
-                        <Form.Group>
-              
-                        <FormControl
-                         name="email"
-                         onChange={handleChange}
-                         onBlur={handleBlur}
-                         isInvalid={errors.email}
-                        className="input-f col" placeholder="ENTER YOUR EMAIL" />
-                            <Form.Control.Feedback type='invalid' className="ml-3 signup-form-control-feedback">
-                                            {touched.email ? errors.email : null}
-                                        </Form.Control.Feedback>
-                      
-                 
-                    
-                        
-                    
-                            <Button
-                            onClick={(e) => {
-                                e.preventDefault();
+                    <Formik
+                        initialValues={formInitialValues}
+                        validationSchema={validationSchema}
+                        onSubmit={(values) => {
 
-                                handleSubmit();
-                            }}
-                            // disabled={!errors.email}
-                            
-                            className="submit-btn-f col">Subscribe</Button>
-                     </Form.Group>
-                    </div>
-            </Form>
-                          )
+                            onFormSubmit(values);
                         }}
-                </Formik>
-            </div>
-            <hr className="separator" />
-            <div className="footer-bottom-portion">
-                <div className="heading-portion">
-                        KRYPTO <div className="title2">CARDS</div> 
+                    >
+                        {
+                            ({ handleChange, handleBlur, handleSubmit, values, touched, errors }) => {
+
+                                return (
+                                    <Form className="form-sec">
+                                        <Form.Label className="heading">GET OUR LATEST <span className="other-color">UPDATES</span></Form.Label>
+                                        <div className="custom-row">
+                                            <div className="custom-col">
+                                                <FormControl
+                                                    name="email"
+                                                    onChange={handleChange}
+                                                    onBlur={handleBlur}
+                                                    isInvalid={errors.email}
+                                                    className="input-f col" placeholder="ENTER YOUR EMAIL" />
+
+
+                                                <Form.Control.Feedback type='invalid' className="ml-3 signup-form-control-feedback">
+                                                    {touched.email ? errors.email : null}
+                                                </Form.Control.Feedback>
+                                            </div>
+
+
+
+                                            <Button
+                                                onClick={(e) => {
+                                                    e.preventDefault();
+
+                                                    handleSubmit();
+                                                }}
+                                   
+
+                                                className="submit-btn-f">Subscribe</Button>
+
+
+
+                                        </div>
+                                    </Form>
+                                )
+                            }}
+                    </Formik>
                 </div>
-                <div className="terms-portion">
-                &copy; All Rights Reserved
+                <hr className="separator" />
+                <div className="footer-bottom-portion">
+                    <div className="heading-portion">
+                        KRYPTO <div className="title2">CARDS</div>
+                    </div>
+                    <div className="terms-portion">
+                        &copy; All Rights Reserved
+                </div>
                 </div>
             </div>
-        </div>
 
         </Container>
     )
 }
 
 export default Footer
+
+
+
+
