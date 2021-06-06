51c35fc9d8e2a8d7a6dfbf97551bd7d7787f6e83
Aryamann Ningombam <aryamannsingh9@gmail.com>
2021-06-06 14:40:59 +0530

    Test

--- client/src/Components/Categories.jsx
+++ client/src/Components/Categories.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import "./Categories.scss";
 import { CategoriesCard } from './CategoriesCard';
+import {BACKEND_URL} from '../constants';
+
+
 export const Categories = () => {
+    const basketballSVG = BACKEND_URL + '/public/assets/Logos/basketball_svg.svg'
+    const footballSVG = BACKEND_URL + '/public/assets/Logos/football_svg.svg'
+    const animeSVG = BACKEND_URL + '/public/assets/Logos/anime_svg.svg'
     return (
         <div className="categories-main-div">
             <div className="main-heading">our <div className="color-primary">
                 categories</div></div>
             <div className="categories-cards">
-                <CategoriesCard/>
-                <CategoriesCard/>
-                <CategoriesCard/>
-                <CategoriesCard/>
+                <CategoriesCard image={basketballSVG} text="BASKETBALL"/>
+                <CategoriesCard image={basketballSVG} text="BASKETBALL"/>
+                <CategoriesCard image={animeSVG} text="ANIME"/>
+                <CategoriesCard image={footballSVG} text="FOOTBALL"/>
            
             </div>
         </div>
--- client/src/Components/CategoriesCard.jsx
+++ client/src/Components/CategoriesCard.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import "./CategoriesCard.scss";
-import {BACKEND_URL} from '../constants';
+
 import Tilt from 'react-tilt';
 
 
 
-export const CategoriesCard = () => {
-    const basketballSVG = BACKEND_URL + '/public/assets/Logos/basketball_svg.svg'
+export const CategoriesCard = ({image,text}) => {
+    
     return (
         <Tilt className="categories-card-main-div" options={{ max : 45 }}  >
-          <img className="image" src={basketballSVG}/>
-            BASKETBALL 
+            <div className="image-wrapper">
+            <img className="image" src={image}/>
+            </div>
+          
+            <div className="text">
+            {text} 
+            </div>
+            
        </Tilt>
     )
 }
