c22777f4b3cc7657382d5377a7d71fbf96d576fa
Aryamann Ningombam <aryamannsingh9@gmail.com>
2021-06-06 12:28:52 +0530

    TEst'

--- client/src/Components/CategoriesCard.jsx
+++ client/src/Components/CategoriesCard.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import "./CategoriesCard.scss";
 import {BACKEND_URL} from '../constants';
+import Tilt from 'react-tilt';
+
+
+
 export const CategoriesCard = () => {
-  
+    const basketballSVG = BACKEND_URL + '/public/assets/Logos/basketball_svg.svg'
     return (
-    <>
-        <div className="categories-card-main-div">
-        
-        </div>
-    </>
+        <Tilt className="categories-card-main-div" options={{ max : 45 }}  >
+          <img className="image" src={basketballSVG}/>
+            BASKETBALL 
+       </Tilt>
     )
 }
