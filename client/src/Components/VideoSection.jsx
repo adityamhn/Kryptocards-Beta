79fce0d9cd71a0be7d87091203c7a6aa88598ce6
kunal bhatia <kunal18042002@gmail.com>
2021-06-06 16:58:55 +0530

    f

--- client/src/Components/VideoSection.jsx
+++ client/src/Components/VideoSection.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const VideoSection = () => {
     return (
         <div>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/05nRA6C3xqo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/05nRA6C3xqo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
         </div>
     )
 }
