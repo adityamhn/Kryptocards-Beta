a76b946781e7051eb1767c0514848ed1cb844400
kunal bhatia <kunal18042002@gmail.com>
2021-06-06 13:52:40 +0530

    added all the cards and made improvements

--- client/src/Components/TierSection.jsx
+++ client/src/Components/TierSection.jsx
@@ -8,7 +8,7 @@ export const TierSection = () => {
     const goldCard  =  BACKEND_URL  + '/public/assets/cards/gold.svg';
     const diamondCard  =  BACKEND_URL  + '/public/assets/cards/diamond.svg';
     const sapphireCard  =  BACKEND_URL  + '/public/assets/cards/sapphire.svg';
-    const emeraldCard  =  BACKEND_URL  + '/public/assets/cards/Group 55-1.svg';
+    const emeraldCard  =  BACKEND_URL  + '/public/assets/cards/emerald.svg';
     const diamondTorch  =  BACKEND_URL  + '/public/assets/Torches/diamond_torch.svg'
     const silverTorch  =  BACKEND_URL  + '/public/assets/Torches/sapphire_torch.svg'
     const goldTorch  =  BACKEND_URL  + '/public/assets/Torches/golde_torch.svg'
