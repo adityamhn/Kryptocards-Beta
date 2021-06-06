0d008673af95e979465ed12428a25307174b92f2
Aryamann Ningombam <aryamannsingh9@gmail.com>
2021-06-06 11:58:11 +0530

    Tets

--- client/src/Components/Categories.jsx
+++ client/src/Components/Categories.jsx
@@ -11,8 +11,7 @@ export const Categories = () => {
                 <CategoriesCard/>
                 <CategoriesCard/>
                 <CategoriesCard/>
-                <CategoriesCard/>
-                <CategoriesCard/>
+           
             </div>
         </div>
     )
